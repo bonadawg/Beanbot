af66b2575f5254583d11a5923d24dbf7ca0d5d63
James Bonadonna <jamesbonadonna@gmail.com>
2020-12-06 22:42:06 -0600

    done?

--- beanbot.py
+++ beanbot.py
@@ -7,6 +7,7 @@ import boto3
 #from PIL import Image
 import io
 import aiohttp
+import random
 
 load_dotenv()
 TOKEN = os.environ['DISCORD_TOKEN']
@@ -18,19 +19,19 @@ b = 'beanimages'
 
 aws_client = boto3.client('s3', aws_access_key_id=client_id, aws_secret_access_key=client_secret)
 
-aws_client.list_objects_v2(Bucket=b)
+obj = aws_client.list_objects_v2(Bucket=b)
 
-jpg = aws_client.get_object(Bucket=b,Key='bean.jpg')['Body']
+beans = obj['Contents']
 
-base_url=f'https://{b}.s3.us-east-2.amazonaws.com/'
+def get_bean():
 
-img_data = base_url+'bean.jpg'
+    pic_name = beans[random.randint(0, len(beans)-1)]['Key']
 
+    base_url=f'https://{b}.s3.us-east-2.amazonaws.com/'
 
-#print(img_data)
+    img_data = base_url+pic_name
 
-#return Image.open(io.BytesIO(img_data))
-
+    return img_data
 
 @client.event
 async def on_ready():
@@ -43,19 +44,28 @@ async def on_message(message):
         return
 
     if message.content == '$bean':
-        #response = img_data
-        #await message.channel.send(response)
-        #discord.Embed().set_image(url=img_data)
+        img_data = get_bean()
         async with aiohttp.ClientSession() as session:
             async with session.get(img_data) as resp:
                 if resp.status != 200:
                     return await channel.send('Could not download file...')
                 data = io.BytesIO(await resp.read())
-                await message.channel.send(file=discord.File(data, 'cool_image.png'))
+                await message.channel.send(file=discord.File(data, img_data))
 
     if message.content == '$beanbomb':
-        response = 'Not yet'
-        await message.channel.send(response)
+        s = set()
+        for _ in range(5):
+            curr = get_bean()
+            while curr in s:
+                curr = get_bean()
+            s.add(curr)
+
+            async with aiohttp.ClientSession() as session:
+                async with session.get(curr) as resp:
+                    if resp.status != 200:
+                        return await channel.send('Could not download file...')
+                    data = io.BytesIO(await resp.read())
+                    await message.channel.send(file=discord.File(data, curr))
 
     if message.content == '$beannuke':
         response = 'No.'
