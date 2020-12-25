9b0fd654fbe466273a406b6e08c06c7914f44289
James Bonadonna <jamesbonadonna@gmail.com>
2020-12-25 02:41:19 -0500

    beans

--- beanbot.py
+++ beanbot.py
@@ -71,7 +71,7 @@ async def on_message(message):
         response = 'No.'
         await message.channel.send(response)
 
-    if message.content == '$beannihilate':
+    if message.content == '$beannihilate' or message.content.split().length == 2:
         s = set()
         for _ in range(15):
             curr = get_bean()
